fc072c73966b3fafb2290d0a2b5dfa09a46bfcfa
ZZ17390 <kaeru_yuya@hotmail.com>
2019-09-30 18:18:07 +0900

    First Commit

--- loadmap/copy_listname.py
+++ loadmap/copy_listname.py
@@ -1,22 +1,22 @@
 import os
 import shutil
 
-mode = 'Emode90'
-path = "C:/Users/zz17807/OneDrive - Komatsu Ltd/共同研究/負荷情報検証/ポンプ圧/"+mode+"%/"
+mode = 'Lmode10'
+path = "C:/Users/zz17807/OneDrive - Komatsu Ltd/共同研究/負荷情報検証/温度/水温/"+mode+"%/"
 sidlist = path+'PC200_loadmap_'+mode+'.csv'
 
-spath = "C:/Users/zz17807/OneDrive - Komatsu Ltd/車両負荷マップ/PC200-10/PC200-10負荷情報_可視化/"
+spath = "C:/Users/zz17807/OneDrive - Komatsu Ltd/車両負荷マップ/file/"
 mpath = "C:/Users/zz17807/OneDrive - Komatsu Ltd/共同研究/190819/負荷情報/"
 
 with open(sidlist, 'r') as f:
     for line in f:
-        sid = line.replace('\n', '')
-        fd = spath+sid+'/loadmap/'
+        sid = line.replace('\n', '').replace('-10-', '-10- -')
+        fd = spath
         root = os.listdir(fd)
         for load in root:
-            if 'エンジン水温' in load :
-                if 'VS作動油温.csv.png' not in load:
-                    shutil.copyfile(fd+load, path+sid+'_'+load)
+            if sid in load:
+                if '_エンジン水温.csv' in load :
+                    shutil.copyfile(fd+load, path+load)
 
         #root = os.listdir(mpath)
         #for master in root:
